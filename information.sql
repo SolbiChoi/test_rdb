3375d25c85a9ff95ac3ee8267685ceb0a8983a74
solbi <csb1422@naver.com>
2021-07-30 13:42:13 +0900

    basic query

---
+++ information.sql
@@ -0,0 +1,37 @@
+create table information(
+    id text,
+    name text
+);
+
+create table information(id text, name text);
+
+insert into information(id,name) values ('1', '홍길동');
+
+insert into information(name, id)
+    values('장길산', '2');
+
+insert into information(name) values('정몽길')
+
+select id, name from information
+--where id is not null;  NULL
+--where id is null;
+--where id = '2;  문자
+--where id != '2;
+where name like '%길%'
+--where id >= '2;  숫자
+
+select id, name from information
+where id is null;
+
+delete from information
+where id is null;
+
+select id, name from information
+--where id is null;
+
+delete from information
+where name like '%길%'
+
+select id, name from information
+
+drop table information
